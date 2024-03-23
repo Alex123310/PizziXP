2b81171597035b6b280c70c2d10e6f44dfde5807
Alex123310 <alessandrett@gmail.com>
2024-03-23 23:03:35 +0100

    Refactored GUI code and added user input for interface selection

--- app/src/main/java/pizzixp/App.java
+++ app/src/main/java/pizzixp/App.java
@@ -4,55 +4,34 @@
 package pizzixp;
 //Usually you will require both swing and awt packages
 // even if you are working with just swings.
-import javax.swing.*;
-import java.awt.*;
+import java.util.Scanner;
 class gui 
 {
     public static void main(String args[]) 
     {
-
-
-        //Creating the Frame
-        JFrame frame = new JFrame("Gay frame");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-
-        //Creating the MenuBar and adding components
-        JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("FILE");
-        JMenu m2 = new JMenu("Help");
-        mb.add(m1);
-        mb.add(m2);
-        JMenuItem m11 = new JMenuItem("Open");
-        JMenuItem m12 = new JMenuItem("Save as");
-        m1.add(m11);
-        m1.add(m12);
-
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
-
-
-        //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        /*----Cancella Dopo----Start*/
-        var mb1 = new JMenuBar();
-        var mb11 = new JMenu("TEST DI UNA SECONDA BARRA");
-        mb1.add(mb11);
-        frame.getContentPane().add(BorderLayout.SOUTH, mb1);
-        var mb3 = new JMenuBar();
-        var mb31 = new JMenu("TEST DI UNA TERZA BARRA");
-        mb3.add(mb31);
-        frame.getContentPane().add(BorderLayout.CENTER, mb3);
-        /*---Cancella Dopo-------End*/
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
-        frame.setVisible(true);
+        int scelta = 0;
+        while (scelta == 0)
+        {
+            System.out.println("Scegli il tipo di interfaccia grafica da utilizzare:");
+            scelta = Integer.parseInt(new Scanner(System.in).nextLine());
+            if (scelta == 1)
+            {
+                new JavaSwingGui();
+                break;
+            }
+            if(scelta == 2)
+            {
+                new awtgui();
+                break;
+            }
+            else
+            {
+                System.out.println("Scelta non valida, riprova");
+                
+                scelta = 0;
+            }
+        }
+        
     }
+
 }
---
+++ app/src/main/java/pizzixp/awtgui.java
@@ -0,0 +1,20 @@
+package pizzixp;
+
+import java.awt.*;
+
+public class awtgui extends Frame
+{
+    awtgui()
+    {
+        Button b = new Button("click me");
+        b.setBounds(30, 100, 80, 30);// setting button position
+        add(b);//adding button into frame
+        setSize(300, 300);//frame size 300 width and 300 height
+        setLayout(null);//no layout manager
+        MenuBar mb = new MenuBar();
+        mb.add(new Menu("File"));
+        setMenuBar(mb);
+        setVisible(true);//now frame will be visible, by default not visible
+    }
+ 
+}
