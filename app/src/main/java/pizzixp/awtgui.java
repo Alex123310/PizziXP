2f55fcea0aa2abd552a24efc38d150a100f0d02d
Alex123310 <alessandrett@gmail.com>
2024-03-25 18:09:39 +0100

    Add button click event and window closing functionality

--- app/src/main/java/pizzixp/awtgui.java
+++ app/src/main/java/pizzixp/awtgui.java
@@ -1,11 +1,13 @@
 package pizzixp;
 
 import java.awt.*;
+import java.awt.event.*;
 
 public class awtgui extends Frame
 {
     awtgui()
     {
+
         Button b = new Button("click me");
         b.setBounds(30, 100, 80, 30);// setting button position
         add(b);                                       //adding button into frame
@@ -14,7 +16,19 @@ public class awtgui extends Frame
         MenuBar mb = new MenuBar();
         mb.add(new Menu("File"));
         setMenuBar(mb);
+
+
+         // Add a WindowListener that listens for the windowClosing event
+         addWindowListener(new WindowAdapter()
+         {
+             public void windowClosing(WindowEvent we)
+             {
+                 System.exit(0);
+             }
+         });
+
         setVisible(true);              //now frame will be visible, by default not visible
+        
     }
  
 }
