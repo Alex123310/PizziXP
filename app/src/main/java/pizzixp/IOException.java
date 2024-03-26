2c7af72fdf356ad6419472f669f3874f5b2dfa99
Alex123310 <alessandrett@gmail.com>
2024-03-26 19:55:08 +0100

    Add menu bar and save functionality to Notepad

---
+++ app/src/main/java/pizzixp/IOException.java
@@ -0,0 +1,5 @@
+package pizzixp;
+
+public class IOException {
+
+}
--- app/src/main/java/pizzixp/Notepad.java
+++ app/src/main/java/pizzixp/Notepad.java
@@ -2,7 +2,9 @@ package pizzixp;
 
 import javax.swing.*;
 import java.awt.Toolkit;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.filechooser.*;
 public class Notepad
 {
     Notepad ()
@@ -14,6 +16,66 @@ public class Notepad
         //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         var w = width / 2;       var h = height / 2;
         frame.setSize(w,h);
+
+
+        //Menu bar
+        JMenuBar mb = new JMenuBar();
+        JMenu FileMenu = new JMenu("FILE");
+        var CloseMenuItem = new JMenuItem("Close");
+        var ExitMenuItem = new JMenuItem("Exit");
+
+        FileMenu.add(CloseMenuItem);
+        FileMenu.add(ExitMenuItem);
+
+        mb.add(FileMenu);
+        frame.setJMenuBar(mb);
+        //Text area
+        var NotepadPanel = new JPanel();        
+        JTextArea textArea = new JTextArea();
+        NotepadPanel.add(textArea);
+
+        //Save Text area to file
+        var SaveMenuItem = new JMenuItem("Save");
+        FileMenu.add(SaveMenuItem);
+
+
+        frame.add(NotepadPanel);
+
+
+        CloseMenuItem.addActionListener(e -> 
+        {
+            frame.dispose();
+        });
+
+        ExitMenuItem.addActionListener(e -> 
+        {
+            System.exit(0);
+        });
+
+        
+        SaveMenuItem.addActionListener(e -> 
+        {
+            var text = textArea.getText();
+            var fileChooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+            fileChooser.setFileFilter(filter);
+            var returnValue = fileChooser.showSaveDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION)
+            {
+                var file = fileChooser.getSelectedFile();
+                try
+                {
+                    var writer = new FileWriter(file);
+                    writer.write(text);
+                    writer.close();
+                }
+                catch (IOException ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        
         frame.setVisible(true);
         
     }
